1fa095a01134c10628cd6a094148555ba1ad33c3
ken Jones <kenjones620@yahoo.co.uk>
2016-03-17 12:46:00 +0000

    fixed bug that allowed you to send a friends request multiple times from the profile page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap
 //= require jquery_ujs
 //= require jquery.turbolinks
-//= require bootstrap
 //= require_tree .
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,12 @@
 class ProfilesController < ApplicationController
   
   def show
+    @friends = current_user.friendships.pluck(:friend_id)
+    @friends_inv = current_user.inverse_friendships.pluck(:user_id)
+    @the_ids = []
+    @the_ids << @friends
+    @the_ids << @friends_inv
+    @friend_ids = @the_ids.flatten
     @user = User.find_by_id(params[:id])
     @thoughts = @user.thoughts.order('created_at DESC')
     @thought = current_user.thoughts.build
