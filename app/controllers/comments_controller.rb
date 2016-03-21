196804177ef882579fc0f92c738f3ce0ef3297e6
ken Jones <kenjones620@yahoo.co.uk>
2016-03-21 14:55:43 +0000

    ajax added to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,22 +4,32 @@ class CommentsController < ApplicationController
         @thought = Thought.find_by_id(params[:thought_id])
         @comment = @thought.comments.create(params[:comment].permit(:content))
         @comment.user_id = current_user.id
-        if @comment.save
-            flash[:success] = "Comment Posted!"
-        else
-            flash[:warning] = "Uh oh! Error!"
+        respond_to do |format|
+            format.html {
+                 if @comment.save
+                    flash[:success] = "Comment Posted!"
+                 else
+                    flash[:warning] = "Uh oh! Error!"
+                 end
+                redirect_to thought_path(@thought)
+                }
+            format.js { @comment.save }    
         end
-        redirect_to thought_path(@thought)
     end
     
     def destroy
         @thought = Thought.find_by_id(params[:thought_id])
         @comment = Comment.find_by_id(params[:id])
-        if @comment.destroy
-            flash[:success] = "Comment Destroyed!"
-        else
-            flash[:warning] = "Uh oh! Error!"
+        respond_to do |format|
+            format.html {
+               if @comment.destroy
+                flash[:success] = "Comment Destroyed!"
+               else
+                flash[:warning] = "Uh oh! Error!"
+               end
+               redirect_to thought_path(@thought) 
+              }
+            format.js { @comment.destroy }  
         end
-        redirect_to thought_path(@thought)
     end
 end
