602b629e7ee0801c91de729c01075da59b35a9a3
ken Jones <kenjones620@yahoo.co.uk>
2016-03-26 00:49:15 +0000

    added some more controller tests

--- test/controllers/my_network_controller_test.rb
+++ test/controllers/my_network_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MyNetworkControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  include Devise::TestHelpers
+  
+  
+  test "should not get show page if user isnt signed in" do 
+    get 'show'
+    assert_response :redirect
+    assert_redirected_to '/users/sign_in'
+  end
+  
+  test "should get show page if user signed in" do 
+    sign_in users(:me)
+    get 'show'
+    assert_response :success
+    assert_template 'show'
+    assert_template layout: 'layouts/application'
+  end
 end
