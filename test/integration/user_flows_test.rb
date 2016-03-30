703dda38a2ac0876779b9594934669a887553690
ken Jones <kenjones620@yahoo.co.uk>
2016-03-30 23:34:38 +0000

    downloaded capybara gem for integration testing, built out some user flow tests

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -1,24 +1,27 @@
 require 'test_helper'
+require 'capybara/rails'
 
 class UserFlowsTest < ActionDispatch::IntegrationTest
   
   def login_user
-    get user_session_path
-    assert_equal 200, status
     @david = User.create(first_name: "David", last_name: "Smith", email: "david@mail.com", password: Devise::Encryptor.digest(User, "helloworld"))
-    post user_session_path, 'user[email]' => @david.email, 'user[password]' =>  @david.password
-    follow_redirect!
+    visit '/'
+    fill_in "Email", with: @david.email
+    fill_in "Password", with: @david.password
+    click_button('Log in')
+    assert page.has_content?('Signed in successfully.')
   end
   
   test "user logs in and browses site then signs out" do 
-    login_user
-    assert_template 'thoughts/index'
-    get_via_redirect(my_network_show_path)
-    assert_template 'my_network/show'
-    get_via_redirect(profiles_show_path(:id => @david.id))
-    assert_template 'profiles/show'
-    get_via_redirect(root_path)
-    assert_template 'thoughts/index'
+   login_user
+   click_link "Connect"
+   assert page.has_content?("#{users(:amy).first_name}")
+   click_link "Profile"
+   assert page.has_content?("#{@david.first_name}'s Thoughts")
+   click_link "Feed"
+   assert page.has_css?('#feed_loop')
+   click_link "Sign Out"
+   assert_equal current_path, user_session_path
   end
-   
+  
 end
