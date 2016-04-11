6f8ea2f572f02d2771a408780f17ee265df9b067
ken Jones <kenjones620@yahoo.co.uk>
2016-04-11 21:33:57 +0000

    likes are now fully functional with ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,39 @@
 class LikesController < ApplicationController
 
-  def create
+def create
+    @thought = Thought.find_by_id(params[:id])
   	@like = current_user.likes.build(:thought_id => params[:id])
-    if @like.save
-       flash[:success] = 'You liked this post!'
-       redirect_to :back
-    else
-       flash[:warning] = 'There was a problem, try again later!'
-       redirect_to :back
-     end
+  	respond_to do |format|
+  	format.html {  
+      if @like.save
+         flash[:success] = 'You liked this post!'
+         redirect_to :back
+      else
+         flash[:warning] = 'There was a problem, try again later!'
+         redirect_to :back
+      end
+  	}
+  	format.js {
+  	  @like.save
+  	}
   end
+end
 
   def destroy
+    @thought = Thought.find_by_id(params[:id])
     @like = Like.find_by(user_id: params[:user_id], thought_id: params[:thought_id])
-    if @like.destroy
-      flash[:success] = 'Unliked thought!'
-    else
-      flash[:warning] = 'Whoops, try again later'
+    respond_to do |format|
+      format.html {
+        if @like.destroy
+          flash[:success] = 'Unliked thought!'
+        else
+          flash[:warning] = 'Whoops, try again later'
+        end
+        redirect_to :back
+      }
+      format.js {
+        @like.destroy
+      }
     end
-    redirect_to :back
   end 
 end
