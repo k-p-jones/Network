eeb31b9c49dd075626f04ef518e61ac761d8cbfb
ken Jones <kenjones620@yahoo.co.uk>
2016-04-12 23:34:06 +0000

    Revert "horrible time doing ajax"
    This reverts commit 8fb9fd4be29ceb2eda76eafc46cc24467c6f9bcf.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,62 +3,32 @@ class FriendshipsController < ApplicationController
     before_action :authenticate_user!
     
     def create
-        @user = User.find_by(params[:id])
-        @friend_ids = current_user.my_friends
-        @pending = current_user.friendships.where('friend_id= ?', @user.id)
-        @recieved = current_user.inverse_friendships.where('user_id= ?', @user.id)
-        @confirmed = current_user.confirmed_profile_friendship(@user.id, current_user)
         @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-        respond_to do |format|
-            format.html {
-              if @friendship.save
-                flash[:success] = "Added friend."
-                redirect_to :back
-              else
-                flash[:warning] = "Unable to add friend."
-                redirect_to :back
-              end  
-            }
-            format.js {
-                @friendship.save
-            }
+        if @friendship.save
+            flash[:success] = "Added friend."
+            redirect_to :back
+        else
+            flash[:warning] = "Unable to add friend."
+            redirect_to :back
         end
     end
 
     def destroy
-      @user = User.find_by(params[:id])
-      @friend_ids = current_user.my_friends
-      @recieved = current_user.inverse_friendships.where('user_id= ?', @user.id)
-      @confirmed = current_user.confirmed_profile_friendship(@user.id, current_user)
       @friendship = Friendship.find_by_id(params[:id])
-      respond_to do |format|
-          format.html {
-            @friendship.destroy
-            flash[:success] = "Removed friendship."
-            redirect_to :back
-          }
-          format.js {
-                @friendship.destroy
-          }
-      end
+      @friendship.destroy
+      flash[:success] = "Removed friendship."
+      redirect_to :back
     end
     
     def update
         @friendship = current_user.inverse_friendships.find_by_id(params[:id])
         @friendship.update_attributes(accepted: true)
-        respond_to do |format|
-            format.html {
-                if @friendship.save
-                    flash[:success] = "You are now friends!"
-                else
-                    flash[:warning] = "There was a problem"
-                end
-                redirect_to :back        
-            }
-            format.js {
-                @friendship.save
-            }
+        if @friendship.save
+            flash[:success] = "You are now friends!"
+        else
+            flash[:warning] = "There was a problem"
         end
+        redirect_to :back
     end
     
 end
