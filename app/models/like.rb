712ffe7633c0102f72cbe70af4ea1fb117cf05e2
ken Jones <kenjones620@yahoo.co.uk>
2016-04-15 13:24:10 +0000

    added model tests for likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,5 @@ class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :thought
   validates_uniqueness_of :user_id, :scope => :thought_id
+  validates :user_id, presence: true
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should save with valid user_id" do 
+    @thought = thoughts(:one)
+    @thought.likes.build(:user_id => users(:me).id)
+    assert @thought.save
+  end
+  
+  test "should not save with invalid user id" do 
+    @thought = thoughts(:one)
+    @thought.likes.build(:user_id => "")
+    assert_not @thought.save
+  end
+  
+  test "like should be unique" do 
+    @thought = thoughts(:one)
+    @thought.likes.build(:user_id => users(:me).id)
+    @thought.save
+    @thought.likes.build(:user_id => users(:me).id)
+    assert_not @thought.save
+  end
+  
+  test "like should belong to a user" do 
+    @thought = thoughts(:one)
+    @thought.likes.build(:user_id => users(:me).id)
+    assert @thought.likes.first.user_id == users(:me).id
+  end 
+  
+  test "like should belong to a thought" do 
+    @thought = thoughts(:one)
+    @like = @thought.likes.build(:user_id => users(:me).id)
+    assert @like.thought_id == thoughts(:one).id
+  end
 end
